dee6a9ad08ba92e11b3037614644cdb3f7c2d0ae
diiiiieegooo <123724300+diiiiieegooo@users.noreply.github.com>
2023-04-18 12:08:12 +0200

    cambios

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ nombre = nombre.toLowerCase().replace(/\s+/g, '');
 var nombreAlReves = '';
 
 for (var i = nombre.length - 1; i >= 0; i--) {
-  nombreAlReves += nombre[i];
+    nombreAlReves += nombre[i];
 }
 
 alert("¡Adiós " + nombreAlReves + "!");
